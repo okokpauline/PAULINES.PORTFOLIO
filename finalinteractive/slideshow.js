529a221128c69fcebed7a93d5ad1284ea5b04cd4
Pauline Cenizal <paulinestyles1@gmail.com>
2023-12-17 20:25:19 -0800

    fix some change on slideshow

--- finalinteractive/slideshow.js
+++ finalinteractive/slideshow.js
@@ -1,8 +1,18 @@
 'use strict';
 
+const resetBorders = () => {
+  const slideshow = document.querySelectorAll('.slideshow');
+  slideshow.forEach((slide) => {
+    slide.style.border = '1px solid azure';
+  });
+};
+
 const changeSlide = (event, slideshow) => {
-  const slideToSwitchTo = event.currentTarget.className.split(' ')[1];
-  $('hero').style.backgroundImage = `url('/images/slideshow/${slideToSwitchTo}.jpg')`;
+  resetBorders();
+  const slideToSwitchTo = event.currentTarget;
+  const slideName = slideToSwitchTo.className.split(' ')[1];
+  slideToSwitchTo.style.border = '2px solid black';
+  $('hero').style.backgroundImage = `url('/images/slideshow/${slideName}.svg')`;
 };
 
 document.addEventListener('DOMContentLoaded', () => {
